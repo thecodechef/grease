7a815b46440a5eb018456c409578ca6d89be5f44
Jeremy Bolding <cyberchefjay@gmail.com>
2018-12-27 23:57:52 -0600

    Added some stuff to spec/spec_helper.rb

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,21 +1,41 @@
-require 'simplecov'
-require 'rspec/simplecov'
+# frozen_string_literal: true
 
+if ENV['REPORTER_ID'] || ENV['CI']
+  require 'simplecov'
+  require 'coveralls'
 
-SimpleCov.profiles.define "grease" do
+  SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
+    SimpleCov::Formatter::HTMLFormatter,
+    Coveralls::SimpleCov::Formatter
+  ]
 
-  add_group "Binaries", "/bin/"
-  add_group "Features", "/features/"
-  add_group "Libraries", "/lib/"
-  add_group "Specs", "/spec/"
-
+  SimpleCov.minimum_coverage 95
+  SimpleCov.start do
+    command_name 'spec'
+    add_filter 'spec'
+  end
 end
 
-SimpleCov.minimum_coverage 95
-SimpleCov.start "grease"
+require 'bundler/setup'
+require 'rspec'
+require 'rspec/benchmark'
 
-RSpec::SimpleCov.start
+RSpec.configure do |c|
+  c.include(RSpec::Benchmark::Matchers)
 
-# RSpec.configure do |c|
-#   $original_rspec_configuration = c
-# end
+  c.expect_with :rspec do |expect|
+    expect.syntax = :expect
+  end
+
+  c.disable_monkey_patching!
+
+  c.warnings = true
+
+  if c.files_to_run.one?
+    c.default_formatter = 'doc'
+  end
+
+  c.profile_examples = 2
+
+  $original_rspec_configuration = c
+end
